8d28f55bc5c2683152c9620d6dfb35442a8fa7e7
WangFiona <fionaw5th@gmail.com>
2021-09-15 21:49:39 -0700

    Adding comments

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -7,43 +7,45 @@ using namespace std;
   This program runs a guessing game that allows the player to guess a number
   between 0 and 100 (inclusive)
  */
+
 int main()
 {
-  //Initializing variables
+  //Initializing the variables
   int guess = 0;
   int number = 1;
   bool playing = true;
-  char yn;
+  char yn = 'y';
 
-  //While loop runs until player no longer wants to play
+  //A while loop runs until the player no longer wants to play
   while (playing == true) {
-    //Choosing a random number
+    //Create a random number
     srand(time(NULL));
     int random = rand() % 101;
     
     cout << "Guess the number between 0 and 100 (inclusive)" << endl;
 
-    //do while loop runs until player guesses correctly
+    //A do while loop runs until the player guesses the number correctly
     do {
       cout << "Enter your guess:" << endl;
       cin >> guess;
       if (guess > random) { //When the guess is too high
 	cout << "Your guess is too high" << endl;
 	number++;
-      } else if (guess < random) { //When the guess is to low
+      } else if (guess < random) { //When the guess is too low
 	cout << "Your guess is too low" << endl;
 	number++;
       } else { //When the guess is correct
 	cout << "Congratulations you are correct!" << endl;
 	cout << "Number of guesses: " << number << endl;
+	number = 1;
       }
     } while (guess != random);
 
-    //Ask player if they would like to play again
+    //Ask the player if they would like to play again
     cout << "Would you like to play again (y/n)?" << endl;
     cin >> yn;
 
-    //End the loop if no
+    //End the loop if the player does not want to play again
     if (yn == 'n') {
       playing = false;
     }
