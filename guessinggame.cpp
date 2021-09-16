d3a9d71f1d70690b94492b4da1e7471cfd27a601
WangFiona <fionaw5th@gmail.com>
2021-09-15 21:32:33 -0700

    comments

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -15,27 +15,35 @@ int main()
   bool playing = true;
   char yn;
 
+  //While loop runs until player no longer wants to play
   while (playing == true) {
+    //Choosing a random number
     srand(time(NULL));
     int random = rand() % 101;
+    
     cout << "Guess the number between 0 and 100 (inclusive)" << endl;
+
+    //do while loop runs until player guesses correctly
     do {
       cout << "Enter your guess:" << endl;
       cin >> guess;
-      if (guess > random) {
+      if (guess > random) { //When the guess is too high
 	cout << "Your guess is too high" << endl;
 	number++;
-      } else if (guess < random) {
+      } else if (guess < random) { //When the guess is to low
 	cout << "Your guess is too low" << endl;
 	number++;
-      } else {
+      } else { //When the guess is correct
 	cout << "Congratulations you are correct!" << endl;
 	cout << "Number of guesses: " << number << endl;
       }
     } while (guess != random);
 
+    //Ask player if they would like to play again
     cout << "Would you like to play again (y/n)?" << endl;
     cin >> yn;
+
+    //End the loop if no
     if (yn == 'n') {
       playing = false;
     }
